c574682ace98eeaddfa47e8ef0bdae877c556e1d
Griffin Stepler <54520967+GriffinStepler@users.noreply.github.com>
2021-11-02 14:40:14 -0700

    notes

---
+++ Notes/Week_12/script.js
@@ -0,0 +1,7 @@
+function processEmployee() {
+    // create HTML to create a new table row
+    // within the row, create columns for each data point of an employee
+
+    existingTable = document.getElementById();
+    document.createElement();
+}
