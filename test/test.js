0d9a457bb59197dcc2e8682c8e02a7ab00532a18
Griffin Stepler <griffin.stepler@gmail.com>
2021-11-14 20:50:02 -0700

    notes

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 
 for (let i = 0; i < 4; i++) {
-    let pin = 3;
+    var pin = 3;
     console.log(pin);
 } 
 
 
 for (let j = 0; j < 4; j++) {
-    let pin = 5;
+    var pin = 5;
     console.log(pin);
 } 
